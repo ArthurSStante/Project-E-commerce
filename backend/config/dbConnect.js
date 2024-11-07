10665dfda414d8026c0a2b304909fd9a89e99487
Arthur Saltori Stante <arthursaltori@gmail.com>
2024-11-06 22:41:40 -0300

    Banco de Dados Adicionados

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDatabase = () => {
-  let DB_UR = process.env.DB_URI;
+  let DB_URI = process.env.DB_URI;
 
   mongoose.connect(DB_URI).then((con) => {
     console.log(
