15dd0e5e3666e4ab00a54c7f50225e9b858392e6
Arthur Saltori Stante <arthursaltori@gmail.com>
2024-11-07 01:20:08 -0300

    Criação do Login e Register

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,49 @@
+import User from "../models/user.js";
+import catchAsyncErrors from "../middleware/catchAsyncErrors.js";
+import ErrorHandler from "../utils/errorHandler.js";
+
+//Registrar usuários - /api/v/register
+export const registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  const token = user.getJwtToken();
+
+  res.status(201).json({
+    token,
+  });
+});
+
+//Login de usuários - /api/v1/login
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Por favor, informe o email e password", 400));
+  }
+
+  //Busca o usuário no Banco de Dados
+  const user = await User.findOne({ email }).select("password");
+
+  if (!user) {
+    return next(new ErrorHandler("Por favor, informar o usuário", 401));
+  }
+
+  //Verifica se o password está correto
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Password incorreto", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(201).json({
+    token,
+  });
+});
